814458fcebdb6d911ad42f0e10b4e8b811c92882
Ramon <ramonbhb@gmail.com>
2020-06-05 00:17:16 -0300

    Added content to table

--- consumoapi.js
+++ consumoapi.js
@@ -2,7 +2,7 @@ function pegarDados() {
     let nome = document.getElementById("usuario");
     let seguidores = document.getElementById("seguidores");
     let foto = document.getElementById("foto");
-    let pessoas = document.getElementById("pessoas");
+    let tabela = document.getElementById("tabela-pessoas");
 
     console.log("começando");
     // let usuario = prompt("Qual usuário você deseja os dados?");
@@ -12,10 +12,22 @@ function pegarDados() {
     )
     .then(
         data => {             
-            for (i=0;i<data.length;i++) {
-                pessoas.innerHTML = pessoas.innerHTML +
-                "<h1>"+data[i].name+"</h1><p>"+data[i].phone+"</p>";
-            }
+            data.forEach( pessoa => {
+                    let linha = tabela.insertRow(-1);
+                    // <th>Nome</th> <th>Telefone</th>
+                    let nome = linha.insertCell(0);
+                    let telefone = linha.insertCell(1);                    
+                    //let botao = linha.insertCell(2);                                               
+                    nome.innerHTML = pessoa.name;
+                    telefone.innerHTML = pessoa.phone;                   
+                    //botao.innerHTML = "<button>OI</button>";
+                }
+            )
+
+            // for (i=0;i<data.length;i++) {
+            //     pessoas.innerHTML = pessoas.innerHTML +
+            //     "<td>"+data[i].name+"</h1><p>"+data[i].phone+"</p>";
+            // }
         }
     )
 }
